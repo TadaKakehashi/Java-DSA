24ce744b9392a2d01f50c405afba7512f1c4f11e
Aryan Mohanty <aryan333457@gmail.com>
2024-11-14 22:32:36 +0530

    Using Recursion to print number from 5 to 1

---
+++ recursion1.java
@@ -0,0 +1,16 @@
+//print number from 5 to 1;
+public class recursion1 {
+
+    public static void printNum(int num){
+        if(num == 0){
+            return;
+        }
+        System.out.println(num);
+        printNum(num-1);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        printNum(n);
+    }
+    
+}
