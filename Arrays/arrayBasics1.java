4abbeef4c0c6f923915bd095d545339b05124ccf
Aryan Mohanty <aryan333457@gmail.com>
2025-02-01 23:40:03 +0530

    arrays basics

---
+++ Arrays/arrayBasics1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class arrayBasics1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr1 = new int[10];
+
+        for(int i = 0; i <arr1.length; i++){
+            System.out.print("Enter number for position "+i+ ": ");
+            arr1[i] = sc.nextInt();
+        }
+        System.out.println();
+
+        for(int i=0;i<arr1.length;i++){
+            System.out.print(arr1[i] +" ");
+        }
+    }
+}
