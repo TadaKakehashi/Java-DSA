37a2655753451b9ab542dcc24c8574a659e13821
Aryan Mohanty <aryan333457@gmail.com>
2025-02-21 13:28:06 +0530

    Implementation of DoublyLinkedList
    performing append method on doubly linked list and printing the nodes.data

---
+++ LinkedLists/DoublyLinked List/dll1.java
@@ -0,0 +1,49 @@
+package DoublyLinkedList;
+
+public class dll1 {
+    private Node head;
+    private Node tail;
+    public int length;
+
+    dll1(){
+        head = null;
+        tail = null;
+        length = 0;
+    }
+    public static void main(String[] args) {
+        dll1 list = new dll1();
+        list.appendNode(10);
+        list.appendNode(20);
+        list.appendNode(30);
+        list.printList();
+    }
+
+    private void appendNode(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail= newNode;
+        }
+        else{
+            tail.next = newNode;
+            newNode.prev = tail;
+        }
+        tail = newNode;
+        length++;
+    }
+    private static class Node{
+        private int data;
+        private Node next;
+        private Node prev;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    private void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
