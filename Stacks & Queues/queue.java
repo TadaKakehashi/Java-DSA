b28ae181b3b79c3eb621c2bb0eddd1984f295297
Aryan Mohanty <aryan333457@gmail.com>
2025-03-01 11:18:50 +0530

    Performing:  Dequeue implementation
    Dequeue operation is performed at the head for O(1)
    Enqueue operation is performed at the tail for O(1)
    in LinkedList removal of head takes O(1) and removal of tail takes O(n), but adding a tail takes O(1) so for enqueue we use tail and dequeue we use head removal method.

---
+++ Stacks & Queues/queue.java
@@ -0,0 +1,71 @@
+public class queue {
+    public static class Node{
+        int value;
+        Node next;
+        Node(int value){
+            this.value = value;
+        }
+    }
+    public static class Queue{
+        private Node first;
+        private Node last;
+        private int length;
+
+        public Queue(int value){
+            Node newNode = new Node(value);
+            first = newNode;
+            last = newNode;
+            length = 1;
+        }
+
+        public void enqueue(int value){
+            Node newNode = new Node(value);
+            if(length == 0){
+                first = newNode;
+                last = newNode;
+            }
+            else{
+                last.next = newNode;
+                last = newNode;
+            }
+            length++;
+        }
+
+        public Node dequeue(){
+            if(length == 0){
+                System.out.println("Queue is empty!");
+                return null;
+            }
+            Node temp = first;
+            first = first.next;
+            length--;
+            if(length == 0){
+                last = null;
+            } 
+            return temp;
+        }
+
+        public void printQueue(){
+            Node temp = first;
+            System.out.println("Queue: ");
+            while(temp != null){
+                System.out.print(temp.value + " -> ");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.printQueue();
+        Node dequeueNode = queue.dequeue();
+        if(dequeueNode != null)
+        {
+            System.out.println("Dequeued: "+dequeueNode.value);
+        }
+        queue.printQueue();
+    }
+}
